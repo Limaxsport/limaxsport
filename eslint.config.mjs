07621bbc2bc6f2d3afdb9929e99722628734a4a5
Max <oxim7777@gmail.com>
2025-08-18 09:30:18 +0000

    Встановлено необхідні розширення-редактори

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended'],
+  },
+  prettierConfig,
+]);
